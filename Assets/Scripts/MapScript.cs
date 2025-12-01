b2a44690185d2fe15f31f999a75e640ab307c0ec
Brandon-JB <bbourdage@ltu.edu>
2025-12-01 14:59:04 -0500

    Added more to node creation ui

--- Assets/Scripts/MapScript.cs
+++ Assets/Scripts/MapScript.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class MapScript : MonoBehaviour
 {
     public bool mapActive = false;
     [SerializeField] private GameObject SituationUI;
+
+    [SerializeField] private TextMeshProUGUI xText;
+    [SerializeField] private TextMeshProUGUI yText;
 
 
     // Start is called before the first frame update
@@ -22,8 +26,10 @@ public class MapScript : MonoBehaviour
         if (Input.GetMouseButtonDown(0))
         {
             Vector3 mousePos = Input.mousePosition;
-            Debug.Log(mousePos);
+            xText.text = mousePos.x.ToString();
+            yText.text = mousePos.y.ToString();
             SituationUI.SetActive(true);
+
         }
     }
 
